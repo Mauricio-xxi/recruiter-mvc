a227608cad101df62c9713d2dcd76159ec59b458
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 10:10:12 +0200

    feat: add create candidate service and interface

---
+++ src/RecruiterApp.Application/CreateCandidate/CreateCandidateService.cs
@@ -0,0 +1,20 @@
+﻿using RecruiterApp.Domain.Entities;
+using RecruiterApp.Domain.Interfaces;
+
+namespace RecruiterApp.Application.CreateCandidate
+{
+    public class CreateCandidateService : ICreateCandidateService
+    {
+        private readonly ICandidateRepository _candidateRepository;
+
+        public CreateCandidateService(ICandidateRepository candidateRepository)
+        {
+            _candidateRepository = candidateRepository;
+        }
+
+        public Task CreateCandidateAsync(Candidate candidate)
+        {
+            return _candidateRepository.AddAsync(candidate);
+        }
+    }
+}
---
+++ src/RecruiterApp.Application/CreateCandidate/ICreateCandidateService.cs
@@ -0,0 +1,9 @@
+﻿using RecruiterApp.Domain.Entities;
+
+namespace RecruiterApp.Application.CreateCandidate
+{
+    public interface ICreateCandidateService
+    {
+        Task CreateCandidateAsync(Candidate candidate);
+    }
+}
