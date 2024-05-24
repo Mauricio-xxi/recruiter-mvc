dad0b7b3c545f40704d547beee22dad4981c7beb
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 09:58:29 +0200

    feat: add repositories interfaces at domain, candidate and candidateExperience

---
+++ src/RecruiterApp.Domain/Interfaces/ICandidateExperienceRepository.cs
@@ -0,0 +1,13 @@
+﻿using RecruiterApp.Domain.Entities;
+
+namespace RecruiterApp.Domain.Interfaces
+{
+    public interface ICandidateExperienceRepository
+    {
+        Task<IEnumerable<CandidateExperience>> GetAllAsync();
+        Task<CandidateExperience> GetByIdAsync(int id);
+        Task AddAsync(CandidateExperience candidateExperience);
+        Task UpdateAsync(CandidateExperience candidateExperience);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ src/RecruiterApp.Domain/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,13 @@
+﻿using RecruiterApp.Domain.Entities;
+
+namespace RecruiterApp.Domain.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<IEnumerable<Candidate>> GetAllAsync();
+        Task<Candidate> GetByIdAsync(int id);
+        Task AddAsync(Candidate candidate);
+        Task UpdateAsync(Candidate candidate);
+        Task DeleteAsync(int id);
+    }
+}
