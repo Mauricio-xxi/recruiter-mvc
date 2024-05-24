ca1d2a239af149c484d79afdfc327d5cba957d1a
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 09:47:46 +0200

    feat: add candidateExpience entity domain

---
+++ src/RecruiterApp.Domain/Entities/CandidateExperience.cs
@@ -0,0 +1,18 @@
+﻿namespace RecruiterApp.Domain.Entities
+{
+    public class CandidateExperience : EntityBase
+    {
+        public int IdCandidateExperience{ get; set; }
+        public int IdCandidate { get; set; }
+        public string Company {  get; set; }
+        public string Job { get; set;}
+        public string Description { get; set;}
+        public decimal Salary { get; set;}
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // Relationships
+
+        public Candidate Candidate { get; set; }
+    }
+}
