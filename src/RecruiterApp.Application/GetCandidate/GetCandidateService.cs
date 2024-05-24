39c36da620572cc908af19ac7e84fe4beeb75629
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 10:11:13 +0200

    feat: add get candidate service and interface

---
+++ src/RecruiterApp.Application/GetCandidate/GetCandidateService.cs
@@ -0,0 +1,20 @@
+﻿using RecruiterApp.Domain.Entities;
+using RecruiterApp.Domain.Interfaces;
+
+namespace RecruiterApp.Application.GetCandidate
+{
+    public class GetCandidateService : IGetCandidateService
+    {
+        private readonly ICandidateRepository _candidateRepository;
+
+        public GetCandidateService(ICandidateRepository candidateRepository)
+        {
+            _candidateRepository = candidateRepository;
+        }
+
+        public Task<Candidate> GetCandidateByIdAsync(int IdCandidate)
+        {
+            return _candidateRepository.GetByIdAsync(IdCandidate);
+        }
+    }
+}
---
+++ src/RecruiterApp.Application/GetCandidate/IGetCandidateService.cs
@@ -0,0 +1,9 @@
+﻿using RecruiterApp.Domain.Entities;
+
+namespace RecruiterApp.Application.GetCandidate
+{
+    public interface IGetCandidateService
+    {
+        Task<Candidate> GetCandidateByIdAsync(int IdCandidate);
+    }
+}
