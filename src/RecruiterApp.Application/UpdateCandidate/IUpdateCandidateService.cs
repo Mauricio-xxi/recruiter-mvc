5cea498d94062235ca68a00c789b587936f0b26c
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 10:12:09 +0200

    feat: add update candidate service and interface

---
+++ src/RecruiterApp.Application/UpdateCandidate/IUpdateCandidateService.cs
@@ -0,0 +1,9 @@
+﻿using RecruiterApp.Domain.Entities;
+
+namespace RecruiterApp.Application.UpdateCandidate
+{
+    public interface IUpdateCandidateService
+    {
+        Task UpdateCandidateAsync(Candidate candidate, List<int> experiencesToDelete);
+    }
+}
---
+++ src/RecruiterApp.Application/UpdateCandidate/UpdateCandidateService.cs
@@ -0,0 +1,25 @@
+﻿using RecruiterApp.Domain.Entities;
+using RecruiterApp.Domain.Interfaces;
+
+namespace RecruiterApp.Application.UpdateCandidate
+{
+    public class UpdateCandidateService : IUpdateCandidateService
+    {
+        private readonly ICandidateRepository _candidateRepository;
+        private readonly ICandidateExperienceRepository _experienceRepository;
+        public UpdateCandidateService(ICandidateRepository candidateRepository, ICandidateExperienceRepository experienceRepository)
+        {
+            _candidateRepository = candidateRepository;
+            _experienceRepository = experienceRepository;
+        }
+
+        public Task UpdateCandidateAsync(Candidate candidate, List<int> experiencesToDelete)
+        {
+            foreach (var experienceId in experiencesToDelete)
+            {
+                _experienceRepository.DeleteAsync(experienceId);
+            }
+            return _candidateRepository.UpdateAsync(candidate);
+        }
+    }
+}
