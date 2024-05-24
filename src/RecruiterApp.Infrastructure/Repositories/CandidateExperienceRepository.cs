db5bdae14f0f98be21dce4674459b1031b535126
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 10:09:00 +0200

    feat: add candiade experience repository

---
+++ src/RecruiterApp.Infrastructure/Repositories/CandidateExperienceRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecruiterApp.Domain.Entities;
+using RecruiterApp.Domain.Interfaces;
+using RecruiterApp.Infrastructure.Contexts;
+
+namespace RecruiterApp.Infrastructure.Repositories
+{
+    public class CandidateExperienceRepository : ICandidateExperienceRepository
+    {
+        private readonly RecruiterAppContext _context;
+
+        public CandidateExperienceRepository(RecruiterAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<CandidateExperience>> GetAllAsync()
+        {
+            return await _context.CandidateExperiences.ToListAsync();
+        }
+
+        public async Task<CandidateExperience> GetByIdAsync(int id)
+        {
+            return await _context.CandidateExperiences.FindAsync(id);
+        }
+
+        public async Task AddAsync(CandidateExperience candidateExperience)
+        {
+            await _context.CandidateExperiences.AddAsync(candidateExperience);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(CandidateExperience candidateExperience)
+        {
+            _context.CandidateExperiences.Update(candidateExperience);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var candidateExperience = await _context.CandidateExperiences.FindAsync(id);
+            if (candidateExperience != null)
+            {
+                _context.CandidateExperiences.Remove(candidateExperience);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
