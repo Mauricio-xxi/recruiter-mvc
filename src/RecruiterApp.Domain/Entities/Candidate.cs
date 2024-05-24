6b2c25e5731b7883b53c5884c0f72ff56dc6316a
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 09:47:18 +0200

    feat: add candidates domain entity

---
+++ src/RecruiterApp.Domain/Entities/Candidate.cs
@@ -0,0 +1,16 @@
+﻿namespace RecruiterApp.Domain.Entities
+{
+    public class Candidate : EntityBase
+    {
+        public int IdCandidate { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Email { get; set; }
+
+        // Relationships
+
+        public ICollection<CandidateExperience> CandidateExperiences { get; set; }
+
+    }
+}
