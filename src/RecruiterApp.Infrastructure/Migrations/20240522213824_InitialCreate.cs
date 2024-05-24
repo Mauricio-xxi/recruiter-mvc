380d171abc909202808a3114e95c82defc724017
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 10:05:49 +0200

    feat: add ef creation migration

---
+++ src/RecruiterApp.Infrastructure/Migrations/20240522213824_InitialCreate.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecruiterApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    IdCandidate = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    InsertDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.IdCandidate);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CandidateExperiences",
+                columns: table => new
+                {
+                    IdCandidateExperience = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCandidate = table.Column<int>(type: "int", nullable: false),
+                    Company = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Job = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    BeginDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    InsertDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifyDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CandidateExperiences", x => x.IdCandidateExperience);
+                    table.ForeignKey(
+                        name: "FK_CandidateExperiences_Candidates_IdCandidate",
+                        column: x => x.IdCandidate,
+                        principalTable: "Candidates",
+                        principalColumn: "IdCandidate",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CandidateExperiences_IdCandidate",
+                table: "CandidateExperiences",
+                column: "IdCandidate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_Email",
+                table: "Candidates",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CandidateExperiences");
+
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
