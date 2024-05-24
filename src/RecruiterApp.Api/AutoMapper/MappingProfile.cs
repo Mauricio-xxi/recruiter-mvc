df7273b19d80c62bfaca6513a4cb8d2071c66756
Álvaro Camacho <37156417+Mauricio-xxi@users.noreply.github.com>
2024-05-24 10:13:27 +0200

    feat: config automapper and add mapping profiles

---
+++ src/RecruiterApp.Api/AutoMapper/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using RecruiterApp.Api.Models;
+using RecruiterApp.Domain.Entities;
+
+namespace RecruiterApp.Api.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CandidateModel, Candidate>();
+            CreateMap<Candidate, CandidateModel>();
+            CreateMap<Candidate, CandidateCreateViewModel>();
+            CreateMap<CandidateCreateViewModel, Candidate>();
+            CreateMap<Candidate, CandidateViewModel>();
+            CreateMap<CandidateViewModel, Candidate>();
+            CreateMap<Candidate, CandidateEditViewModel>();
+            CreateMap<CandidateEditViewModel, Candidate>();
+            CreateMap<CandidateExperience, CandidateExperienceViewModel>();
+            CreateMap<CandidateExperienceViewModel, CandidateExperience>();
+            CreateMap<CandidateExperience, CandidateExperienceEditViewModel>();
+            CreateMap<CandidateExperienceEditViewModel, CandidateExperience>();
+        }
+    }
+}
